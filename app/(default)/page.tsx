3d2d5eeb4eda731856da1c77891fa45649c6a51c
Anandamurugan <anandamurugan@beezlabs.com>
2024-04-21 01:46:29 +0530

    layout restructured

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,29 +1,32 @@
-
 export const metadata = {
   title: 'Sports Academy',
   description: 'Page description',
 }
 
+import PageIllustration from '@/components/page-illustration'
+import Header from '@/components/header'
 import Hero from '@/components/hero'
 import Features from '@/components/features'
 import Newsletter from '@/components/newsletter'
 import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
+import Footer from '@/components/footer'
 
 export default function Home() {
   return (
    // <div style={{ color: 'green', height: '100%',overflow: 'auto', display: 'flex', flexDirection:'column-reverse'
   //}}>
     <>  
-      < Hero />
+      <PageIllustration />
+      <Header/>
+      <Hero />
       <Features />
       <Zigzag />
       <Testimonials />
       <Newsletter />
+      <Footer />
     </>
-   // </div>
-
-    
+   // </div>    
   )
 }
 
